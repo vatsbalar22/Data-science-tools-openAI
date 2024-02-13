20639a3eff4ae358411351cd253ae051bc3c8f71
Vats Balar <141894006+vatsbalar22@users.noreply.github.com>
2024-02-13 11:39:57 +0000

    Open AI

---
+++ makefile
@@ -0,0 +1,17 @@
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+test:
+	python -m pytest -vv test_*.py
+
+format:
+	black *.py oalib/*.py \
+		streamlit-apps/uberDemo.py
+
+refactor: format lint
+
+lint:
+	pylint --disable=R,C --ignore-patterns=test_.*?py *.py oalib/*.py 
+
+all: install lint test
---
+++ questionAnswerCLI.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+An openai api key is required to use this script.
+This uses an advanced GPT-3 model and I also used AI via Github Copilot to write this command-line interface.
+"""
+
+import click
+from oalib.solutions import submit_question
+
+
+@click.command()
+@click.argument("text")
+def main(text):
+    """This is the main function that you ask the OpenAI API a question to get an answer
+
+    example: /.questionAnswerCLI.py "Who won the 2020 Summer Olympics?"
+
+    """
+    print(submit_question(text))
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    main()
